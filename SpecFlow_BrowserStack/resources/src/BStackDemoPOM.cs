49cb67bbc49a5bbf12356ea4332c026a89ba57e6
ssgit-2003 <satyamsharma2321@gmail.com>
2024-08-27 16:05:48 +0100

    Refactoring code

---
+++ SpecFlow_BrowserStack/resources/src/BStackDemoPOM.cs
@@ -0,0 +1,119 @@
+﻿using BrowserStackSDK.Accessibility;
+using NLog.Filters;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlow_BrowserStack.resources.src
+{
+    public class BStackDemoPOM
+    {
+        private readonly IWebDriver webDriver;
+        readonly WebDriverWait wait;
+        // Properties for Web Elements
+
+        private IWebElement _Signin => webDriver.FindElement(By.Id("signin"));
+        private IWebElement UserNameInput => webDriver.FindElement(By.Id("react-select-2-input"));
+        private IWebElement PasswordInput => webDriver.FindElement(By.Id("react-select-3-input"));
+        private IWebElement LoginButton => webDriver.FindElement(By.Id("login-btn"));
+        private IWebElement _Logout => webDriver.FindElement(By.Id("logout"));
+        private IWebElement FavLink => webDriver.FindElement(By.Id("favourites"));
+        private IWebElement ProductFoundMessage => webDriver.FindElement(By.XPath("//div[@class='shelf-container-header']//span"));
+
+        private IWebElement Filter { get; set; }
+        public IWebElement FavIcon { get; private set; }
+        private IWebElement FavProduct { get; set; }
+
+        // Constructor
+        public BStackDemoPOM(IWebDriver driver)
+        {
+            this.webDriver = driver;
+            wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(20));
+        }
+
+
+        // Actions
+
+        // Method to set the filter based on the test value of filter for the products
+        private void SetFilterByText(string filterText)
+        {
+            string dynamicXPath = $"//span[@class='checkmark' and text()='{filterText}']";
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(dynamicXPath)));
+            Filter = webDriver.FindElement(By.XPath(dynamicXPath));
+        }
+
+        // Method to select favorite based on the test value of the model
+        private void SetFavouriteByModel(string modelName)
+        {
+            string dynamicXPath = $"//div[@class='shelf-item']//p[@class='shelf-item__title' and text()='{modelName}']/parent::div/div[@class = 'shelf-stopper']//button";
+            FavIcon = webDriver.FindElement(By.XPath(dynamicXPath));
+        }
+
+        public void SignIn(string username, string password)
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("signin")));
+            _Signin.Click();
+            EnterUserName(username);
+            EnterPassword(password);
+            ClickLoginButton();
+        }
+
+        private void EnterUserName(string username)
+        {
+            UserNameInput.SendKeys(username);
+            UserNameInput.SendKeys(Keys.Return);
+        }
+
+        private void EnterPassword(string password)
+        {
+            PasswordInput.SendKeys(password);
+            PasswordInput.SendKeys(Keys.Return);
+        }
+
+        private void ClickLoginButton()
+        {
+            LoginButton.Click();
+        }
+
+        public void SetFilter(string filterText)
+        {
+            SetFilterByText(filterText);
+            Filter.Click();
+        }
+
+        public void SetFavourite(string modelName)
+        {
+            SetFavouriteByModel(modelName);
+            FavIcon.Click();
+        }
+
+        public void NavigateToFavourites()
+        {
+            FavLink.Click();
+        }
+
+        public string GetFavouriteProductFoundText()
+        {
+            return ProductFoundMessage.Text;
+        }
+
+        // Method to get the favorite product in the favorites list
+        public string GetFavourite(string modelName)
+        {
+            string dynamicXPath = $"//p[@class='shelf-item__title' and text()='{modelName}']";
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(dynamicXPath)));
+            FavProduct = webDriver.FindElement(By.XPath(dynamicXPath));
+            return FavProduct.Text;
+        }
+
+        public void Logout()
+        {
+            _Logout.Click();
+        }
+    }
+}
--- SpecFlow_BrowserStack/resources/src/SampleTest.cs
+++ SpecFlow_BrowserStack/resources/src/SampleTest.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
 using SeleniumExtras.WaitHelpers;
 using System.Threading;
+using SpecFlow_BrowserStack.resources.src;
 
 namespace SpecFlowBrowserStack
 {
@@ -14,12 +15,14 @@ namespace SpecFlowBrowserStack
 	{
 		private readonly IWebDriver _driver;
 		readonly WebDriverWait wait;
+        public BStackDemoPOM bStackDemoPOM;
 
-		public SampleTest()
+        public SampleTest()
 		{
 			_driver = BrowserStackSpecFlowTest.ThreadLocalDriver.Value;
 			wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
-		}
+            bStackDemoPOM = new BStackDemoPOM(_driver);
+        }
 
         [Given(@"the user navigates to ""(.*)""")]
         public void GivenTheUserNavigatesTo(string url)
@@ -31,56 +34,30 @@ namespace SpecFlowBrowserStack
         [Given(@"the user logs in with username ""(.*)"" and password ""(.*)""")]
         public void ThenTheUserLogsInWithUsernameAndPassword(string username, string password)
         {
-            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("signin")));
-            var signIn = _driver.FindElement(By.Id("signin"));
-
-            signIn.Click();
-            Thread.Sleep(2000);
-
-            var userNameInput = _driver.FindElement(By.Id("react-select-2-input"));
-            var passwordInput = _driver.FindElement(By.Id("react-select-3-input"));
-            var loginButton = _driver.FindElement(By.Id("login-btn"));
-
-            userNameInput.SendKeys(username);
-			userNameInput.SendKeys(Keys.Enter);
-            Thread.Sleep(1000);
-            //passwordInput.Click();
-            passwordInput.SendKeys(password);
-            passwordInput.SendKeys(Keys.ArrowDown);  // Moves to the first suggestion
-            passwordInput.SendKeys(Keys.Enter);  // Selects the suggestion
-            Thread.Sleep(1000);
-            loginButton.Click();
+            bStackDemoPOM.SignIn(username, password);
         }
 
         [When(@"the user filters the product view to show ""(.*)"" devices only")]
         public void WhenTheUserFiltersTheProductViewToShowDevicesOnly(string productFilter)
 
         {
-            string filter = $"//span[@class='checkmark' and text()='{productFilter}']";
-            var filterElement = _driver.FindElement(By.XPath(filter));
-            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(filter)));
-            filterElement.Click();
+            bStackDemoPOM.SetFilter(productFilter);
         }
 
         [When(@"the user favorites the ""(.*)"" device by clicking the yellow heart icon")]
         public void WhenTheUserFavoritesTheDeviceByClickingTheYellowHeartIcon(string model)
 
         {
-            var favIcon = _driver.FindElement(By.XPath($"//div[@class='shelf-item']//p[@class='shelf-item__title' and text()='{model}']/parent::div/div[@class = 'shelf-stopper']//button"));
-            favIcon.Click();
+          bStackDemoPOM.SetFavourite(model);
         }
 
         [Then(@"the user verifies that the ""(.*)"" device is listed on the Favorites page")]
         public void ThenTheUserVerifiesThatTheDeviceIsListedOnTheFavoritesPage(string model)
 
         {
-            _driver.FindElement(By.Id("favourites")).Click();
-            var favProduct = _driver.FindElement(By.XPath($"//p[@class='shelf-item__title' and text()='{model}']"));
-            if (favProduct == null)
-            {
-                throw new Exception($"The model '{model}' was not found on the Favorites page.");
-            }
-            Assert.That(favProduct.Text.Contains(model));
+            bStackDemoPOM.NavigateToFavourites();
+            string favProduct = bStackDemoPOM.GetFavourite(model);
+            Assert.That(favProduct.Contains(model));
         }
 	}
 }
